c16fcad4089bd304bc9ad79a3aeca183bacc3b67
Rud-Rostislav <rudrost.se@gmail.com>
2023-04-26 19:57:39 +0300

    homework_32

--- src/com/ua/robot_dreams/homework_32/Main.java
+++ src/com/ua/robot_dreams/homework_32/Main.java
@@ -2,6 +2,7 @@ package com.ua.robot_dreams.homework_32;
 
 public class Main {
     public static void main(String[] args) {
+        // Багатопотоковість. Частина 1
         ThreadRunnable threadRunnable = new ThreadRunnable();
         Thread thread0 = new Thread(threadRunnable);
 
