528c5bc9cf2d0cbc55b0dca472eb5f10c71d2576
Rud-Rostislav <rudrost.se@gmail.com>
2023-04-26 19:58:59 +0300

    homework_32

--- src/com/ua/robot_dreams/homework_32/ThreadRunnable.java
+++ src/com/ua/robot_dreams/homework_32/ThreadRunnable.java
@@ -3,10 +3,10 @@ package com.ua.robot_dreams.homework_32;
 public class ThreadRunnable implements Runnable {
     @Override
     public void run() {
-        System.out.println("| Thread " + Thread.currentThread().getName() + " started. |");
+        System.out.println("| " + Thread.currentThread().getName() + " started. |");
         for (int i = 1; i <= 10; i++) {
             System.out.println("Name: " + Thread.currentThread().getName() + ". Value: " + i + ".");
         }
-        System.out.println("| Thread " + Thread.currentThread().getName() + " finished. |");
+        System.out.println("| " + Thread.currentThread().getName() + " finished. |");
     }
 }
